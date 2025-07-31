6448342e087250eaab8392cb5b600f54cbc6fb28
JSP-ywu <renew1614@gm.gist.ac.kr>
2025-07-31 10:48:35 +0900

    node600 sync

--- scripts/reproduce_train/outdoor.sh
+++ scripts/reproduce_train/outdoor.sh
@@ -12,14 +12,14 @@ data_cfg_path="configs/data/megadepth_trainval_${TRAIN_IMG_SIZE}.py"
 main_cfg_path="configs/edm/outdoor/edm_base.py"
 
 n_nodes=1
-n_gpus_per_node=4
+n_gpus_per_node=6
 torch_num_workers=8
 batch_size=6
 pin_memory=true
 exp_name="depth_fusion_pre_extracted_f16"
-ckpt=""
+ckpt="/workspace/EDM/logs/wandb_logs/depth_fusion_pre_extracted_f16/checkpoints/epoch=22-auc@5=0.553-auc@10=0.711-auc@20=0.827.ckpt"
 pre_extracted_depth=True
-resume=False
+resume=True
 
 python -u ./train.py \
     ${data_cfg_path} \
